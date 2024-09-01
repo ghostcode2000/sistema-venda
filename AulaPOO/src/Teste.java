01c51b614cdb0d44c680d50f3c7a07d358bcf141
Natã Moreira <Nat Moreira@DESKTOP-64D85T9>
2024-08-31 22:11:52 -0500

    Criando a apricação ...

--- AulaPOO/src/Teste.java
+++ AulaPOO/src/Teste.java
@@ -1,9 +1,45 @@
+import java.math.BigDecimal;
 
 public class Teste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Produto produto1 = new Produto();
+		produto1.setId(1L);
+		produto1.setNome("Módulo orientação a objetos");
+		produto1.setValor(BigDecimal.valueOf(100));
+		
+		Produto produto2 = new Produto();
+		produto2.setId(2L);
+		produto2.setNome("Modulo de Spring Boot API REST");
+		produto2.setValor(BigDecimal.valueOf(300));
 
+		
+		Produto produto3 = new Produto();
+		produto3.setId(3L);
+		produto3.setNome("Modulo Angular 8");
+		produto3.setValor(BigDecimal.valueOf(197));
+
+		Produto produto4 = new Produto();
+		produto4.setId(4L);
+		produto4.setNome("Modulo Angular 8");
+		produto4.setValor(BigDecimal.valueOf(250));
+		Venda venda = new Venda();
+		
+		venda.setDescricaoVenda("Venda Curso Formação Java");
+		venda.setEnderecoEntrega("Entrega pelo E-mail");
+		venda.setId(10L);
+		venda.setNomeCliente("Natã Moreira");
+		//venda.setValorTotal(BigDecimal.valueOf(197.00));
+		
+		venda.getListProdutos().add(produto1);
+		venda.getListProdutos().add(produto2);/*Não consigo fazer validações*/
+		venda.addProduto(produto3);/*Posso fazer validações antes de adicionar na lista*/
+		venda.addProduto(produto4);
+	
+		
+		System.out.println("Valor da variável total Venda: " + venda.getValorTotal());
 	}
 
 }
